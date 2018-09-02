e75651ceafe93b3c46958af42c72413123d5544c
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-09-02 12:33:39 +0800

    Update customcraft.php

--- sakura/customcraft.php
+++ sakura/customcraft.php
@@ -4,10 +4,25 @@ namespace sakura;
 
 use pocketmine\Server;
 use pocketmine\item\Item;
-use pocketmine\plugin\PluginBase as Base;
-use pocketmine\inventory\ShapedRecipe as Recipe;
+use pocketmine\plugin\PluginBase;
+use pocketmine\inventory\ShapedRecipe;
+use pocketmine\inventory\ShapelessRecipe;
+use pocketmine\inventory\CraftingManager as CM;
 
-class customcraft extends Base {
-public function onEnable() : void
-{}
+class customcraft extends PluginBase
+{
+  public function onEnable() : void
+  {
+    //ing [] , $result []
+    $recipe = new ShapelessRecipe(Item::get(287), Item::get(341));
+    $recipe->addIngredient(Item::get(287));
+    $recipe->addIngredient(Item::get(287));
+    $recipe->addIngredient(Item::get(287));
+    $recipe->addIngredient(Item::get(287));
+    $recipe->addIngredient(Item::get(287));
+    $recipe->addIngredient(Item::get(287));
+    $recipe->addIngredient(Item::get(287));
+    $recipe->addIngredient(Item::get(287));
+    CM::registerShapelessRecipe($recipe);
+  }
 }
