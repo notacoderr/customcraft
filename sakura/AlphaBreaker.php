d4c51d5d173bbcf70337bb8df423349d9a750efb
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-09-02 14:34:58 +0800

    Update and rename ItemManager.php to AlphaBreaker.php

---
+++ sakura/AlphaBreaker.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace sakura;
+
+use pocketmine\entity\Entity;
+use pocketmine\item\Tool;
+use pocketmine\item\{Item, ItemFactory};
+
+class AlphaBreaker extends Tool {
+	
+	public function __construct(int $meta = 0, $count = 1)
+	{
+		parent::__construct(600, $meta, "Alpha Breaker");
+	}
+	
+	/*
+	public static function init()
+	{
+
+		ItemFactory::registerItem(new AlphaBreaker(600, 0, "Alpha Breaker"), true);
+		Item::initCreativeItems(600);
+	}*/
+	
+	public function getMaxDurability(): int{
+		return 251;
+	}
+
+	public function getMaxStackSize(): int{
+		return 1;
+	}
+	
+	public function getAttackPoints(): int{
+		return 12;
+	}
+}
