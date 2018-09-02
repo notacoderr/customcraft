31c6e3b64e59a93c13ca8b6442edb63a8884ea34
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-09-02 12:04:59 +0800

    Create customcraft.php

---
+++ sakura/customcraft.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace sakura;
+
+use pocketmine\Server;
+use pocketmine\item\Item;
+use pocketmine\plugin\PluginBase as Base;
+use pocketmine\inventory\ShapedRecipe as Recipe;
+
+class customcraft extends Base {
+public function onEnable() : void
+{}
+}
