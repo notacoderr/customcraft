d7cb629d9470957e7be795788a88fae598c7837f
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-09-04 13:08:11 +0800

    Update AlphaBreaker.php

--- src/sakura/items/AlphaBreaker.php
+++ src/sakura/items/AlphaBreaker.php
@@ -2,26 +2,52 @@
 
 namespace sakura\items;
 
-use pocketmine\item\Tool;
-use pocketmine\item\{Item,
-		     ItemFactory};
+use pocketmine\item\TieredTool;
 
-class AlphaBreaker extends Tool {
+use pocketmine\block\Block;
+use pocketmine\block\BlockToolType;
+
+use pocketmine\entity\Entity;
+
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+
+class AlphaBreaker extends TieredTool {
 	
-	public function __construct(int $meta = 0, $count = 1)
-	{
-		parent::__construct(409, $meta, "Alpha Breaker");
+	public function __construct(int $id = 409, int $meta = 0, string $name = "Alpha Breaker", int $tier = 1) {
+		
+		parent::__construct($id, $meta, $name, $tier);
 	}
 	
-	public function getMaxDurability(): int{
-		return 251;
+	public function getBlockToolType() : int{
+		return BlockToolType::TYPE_SWORD;
 	}
-
-	public function getMaxStackSize(): int{
+	
+	public function getAttackPoints() : int{
+		return self::getBaseDamageFromTier($this->tier);
+	}
+	
+	public function getBlockToolHarvestLevel() : int{
 		return 1;
 	}
 	
-	public function getAttackPoints(): int{
-		return 12;
+	public function getMiningEfficiency(Block $block) : float{
+		return parent::getMiningEfficiency($block) * 1.5; //swords break any block 1.5x faster than hand
 	}
+	
+	protected function getBaseMiningEfficiency() : float{
+		return 10;
+	}
+	
+	public function onDestroyBlock(Block $block) : bool{
+		if($block->getHardness() > 0){
+			return $this->applyDamage(2);
+		}
+		return false;
+	}
+	
+	public function onAttackEntity(Entity $victim) : bool{
+		return $this->applyDamage(1);
+	}
+	
 }
