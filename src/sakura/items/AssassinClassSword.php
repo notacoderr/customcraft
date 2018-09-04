48e9fbaf98b1d30d20fbe7a664ff9577118fe89c
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-09-05 00:38:08 +0800

    Create AssassinClassSword.php

---
+++ src/sakura/items/AssassinClassSword.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace sakura\items;
+
+use pocketmine\item\TieredTool;
+
+use pocketmine\block\Block;
+use pocketmine\block\BlockToolType;
+
+use pocketmine\entity\Entity;
+
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+
+class AssassinClassSword extends TieredTool {
+	
+	public function __construct(int $id = 417, int $meta = 0) {
+		
+		parent::__construct($id, $meta, "Assassin Class Sword", TieredTool::TIER_DIAMOND);
+	}
+	
+	public function getBlockToolType() : int{
+		return BlockToolType::TYPE_SWORD;
+	}
+	
+	public function getAttackPoints() : int{
+		return self::getBaseDamageFromTier($this->tier);
+	}
+	
+	public function getBlockToolHarvestLevel() : int{
+		return 1;
+	}
+	
+	public function getMiningEfficiency(Block $block) : float{
+		return parent::getMiningEfficiency($block) * 1.5; //swords break any block 1.5x faster than hand
+	}
+	
+	protected function getBaseMiningEfficiency() : float{
+		return 10;
+	}
+	
+	public function onDestroyBlock(Block $block) : bool{
+		if($block->getHardness() > 0){
+			return $this->applyDamage(2);
+		}
+		return false;
+	}
+	
+	public function onAttackEntity(Entity $victim) : bool{
+		return $this->applyDamage(1);
+	}
+	
+}
