f673eb641f3437f55cdaedd45788a5f803a66b71
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-09-04 09:36:28 +0800

    Update AlphaBreaker.php

--- src/sakura/items/AlphaBreaker.php
+++ src/sakura/items/AlphaBreaker.php
@@ -3,22 +3,15 @@
 namespace sakura\items;
 
 use pocketmine\item\Tool;
-use pocketmine\item\{Item, ItemFactory};
+use pocketmine\item\{Item,
+		     ItemFactory};
 
 class AlphaBreaker extends Tool {
 	
 	public function __construct(int $meta = 0, $count = 1)
 	{
-		parent::__construct(600, $meta, "Alpha Breaker");
+		parent::__construct(409, $meta, "Alpha Breaker");
 	}
-	
-	/*
-	public static function init()
-	{
-
-		ItemFactory::registerItem(new AlphaBreaker(600, 0, "Alpha Breaker"), true);
-		Item::initCreativeItems(600);
-	}*/
 	
 	public function getMaxDurability(): int{
 		return 251;
